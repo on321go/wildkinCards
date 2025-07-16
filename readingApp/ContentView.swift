05ce683d264fb3c7a7df67ead1444cc0f0e2b643
Joey Rubin <on321go@gmail.com>
2025-07-16 10:46:07 -0700

    Initial Commit

---
+++ readingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  readingApp
+//
+//  Created by Joey Rubin on 7/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ readingApp/readingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  readingAppApp.swift
+//  readingApp
+//
+//  Created by Joey Rubin on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct readingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
